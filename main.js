22100fba2526361269fad9265d1c50de852bdde5
rudravaghela <72458643+rudravaghela@users.noreply.github.com>
2021-05-10 20:49:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-nose_X = 0;
-nose_Y = 0;
+eye_X = 0;
+eye_Y = 0;
 
 function preload(){
 
@@ -9,31 +9,32 @@ function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(300,300);
-    video.hide();
-    poseNet = ml5.poseNet(video,modelLoaded);
-    poseNet.on("pose", gotPoses);
+    video.size(300, 300);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+
 }
 
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
+
+  
 function draw(){
-image(video,0,0,300,300);
+  image(video,0,0,300,300);
 } 
 
 function take_snapshot(){
     save("rudra.png");
 }
 
-function modelLoaded(){
-    console.log("modelLoaded!");
-    
-}
-
 function gotPoses(results){
-    if (results.length > 0)
+  if (results.length > 0)
 {
-    console.log(results);
-    console.log("nose x = " +results[0].pose.nose.x);
-    console.log("nose y = "+results[0].pose.nose.y);
-    
+  console.log(results);
+  console.log("right eye x = " +results[0].pose.rightEye.x);
+  console.log("right eye y = "+results[0].pose.rightEye.y);
+  
 }
 }
